f486989d7ccbdb75d33bc1be4db7ab334348dcd2
wangyiwei <1206182848@qq.com>
2024-01-26 16:40:55 +0800

    第一次提交

---
+++ src/main/java/org/example/idea_git_test/IdeaGitTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.idea_git_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaGitTestApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(IdeaGitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/idea_git_test/IdeaGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.idea_git_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaGitTestApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+
+}
