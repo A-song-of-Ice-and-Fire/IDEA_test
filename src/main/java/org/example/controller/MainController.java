8c47bc6a44a3bb820b44aaa38ccd71fb4760cdcd
wangyiwei <1206182848@qq.com>
2024-01-26 17:28:36 +0800

    第二个人做的第二次更改

--- src/main/java/org/example/controller/MainController.java
+++ src/main/java/org/example/controller/MainController.java
@@ -2,7 +2,7 @@ package org.example.controller;
 
 public class MainController{
     {
-        System.out.println("第二个人作的更改");
+        System.out.println("第二个人作的第二次更改");
         // 本地的第四次添加
         // 远端的第二次添加
     }
